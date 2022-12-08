ca38159c2358657de0580eb65a3bdcc6d45d42b5
Philip McGrath <philip@philipmcgrath.com>
2022-12-08 00:19:53 -0500

    relicense this repository under the MPL-2.0

--- info.rkt
+++ info.rkt
@@ -3,21 +3,20 @@
 (define pkg-name "recaptcha")
 (define collection "recaptcha")
 
-(define deps '(("base" #:version "6.1")
-               "web-server-lib"
-               ))
+(define deps
+  '(["base" #:version "6.1"]
+    "web-server-lib"))
 
-(define build-deps '("scribble-lib"
-                     "racket-doc"
-                     "web-server-doc"
-                     ))
+(define build-deps
+  '("scribble-lib"
+    "racket-doc"
+    "web-server-doc"))
 
 (define scribblings
   '(("scribblings/recaptcha.scrbl"
      ()
      (net-library)
-     "reCAPTCHA"
-     )))
+     "reCAPTCHA")))
 
 (define pkg-desc
   "Utilities for using reCAPTCHA with web-server/formlets")
@@ -25,3 +24,6 @@
 (define version "0.1.1")
 
 (define pkg-authors '(philip))
+
+(define license
+  'MPL-2.0)
